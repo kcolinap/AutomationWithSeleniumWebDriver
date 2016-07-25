1d219d6790a1e7ce5ad35d0820dc3090b7a873e0
Alejandro Gil <agil@QA19.consis.local>
2016-07-25 14:15:54 -0430

    Avance en las clases Menu

---
+++ src/main/controller/Menu/MenuMantenimiento.java
@@ -0,0 +1,11 @@
+package main.controller.Menu;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by agil on 25/07/2016.
+ */
+public class MenuMantenimiento {
+
+    protected ThreadLocal<WebDriver> threadDriver = null;
+}
---
+++ src/main/controller/Menu/MenuOperaciones.java
@@ -0,0 +1,278 @@
+package main.controller.Menu;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by agil on 25/07/2016.
+ */
+public class MenuOperaciones {
+
+    protected ThreadLocal<WebDriver> threadDriver = null;
+
+    /** Operaciones **/
+
+    public void OperacionesWorkflow(){
+
+    }
+
+    /** Operaciones Polizas **/
+
+    public void OpePol_CrearWController(){
+
+    }
+
+    public void OpePol_Crear(){
+
+    }
+
+    public void OpePol_CrearCotizacion(){
+
+    }
+
+    public void OpePol_BúsquedaCotizaciones(){
+
+    }
+
+    public void OpePol_SuscripciónMantenimientoPólizasWController(){
+
+    }
+
+    public void OpePol_CotizaciónSuscripciónMantenimientoPólizas(){
+
+    }
+
+    public void OpePol_BúsquedaSolicitud(){
+
+    }
+
+    public void OpePol_AdministracionCuotasComodin(){
+
+    }
+
+    public void OpePol_EmisiónMasivaPropuestas(){
+
+    }
+
+    public void OpePol_RecuperaciónOperacionesPendientesWController(){
+
+    }
+
+    public void OpePol_RecuperaciónOperacionesPendientes(){
+
+    }
+
+            /** OpePol_EndososMasivos **/
+        public void OpePol_EndososMasivos_IncluirExcluirCoberturas(){
+
+    }
+
+        public void OpePol_EndososMasivos_CambioPlan(){
+
+    }
+            /** **/
+
+    public void OpePol_CargaASL(){
+
+    }
+
+    public void OpePol_ÚltimaOperaciónReserva(){
+
+    }
+
+    public void OpePol_ProcesoCancelaciónPorFaltaPago(){
+
+    }
+
+        /** OpePol_PolizasArchivosProcesados**/
+        public void OpePol_ArchivosProcesados_ListaArchivosProcesados(){
+
+        }
+
+        public void OpePol_ArchivosProcesados_ReordenarArchivos(){
+
+        }
+
+        public void OpePol_ArchivosProcesados_AplicarAcciónMasiva(){
+
+        }
+        /** **/
+
+    public void OpePol_BusquedaDocumentos(){
+
+    }
+
+        /** OpePol_PólizasMasivo **/
+        public void OpePol_PólizasMasivo_Importar(){
+
+        }
+
+        public void OpePol_PólizasMasivo_Exportar(){
+
+        }
+
+        public void OpePol_PólizasMasivo_HistóricoPólizasRechazadas(){
+
+        }
+        /** **/
+
+    public void OpePol_CotizacionesPendientes(){
+
+    }
+
+
+
+
+    /** Operaciones Siniestros**/
+
+    public void OpeSini_DeclaraciónSiniestro(){}
+
+    public void OpeSini_MantenimientoSiniestro(){}
+
+    public void OpeSini_CreacionSiniestroFrontEnd(){}
+
+    public void OpeSini_NotificacionSiniestro(){}
+
+    public void OpeSini_MantenimientoSiniestroVaadin(){}
+
+    public void OpeSini_CreaciónSiniestro(){}
+
+    public void OpeSini_Recuperaciones(){}
+
+    public void OpeSini_MantenimientoRecuperación(){}
+
+    public void OpeSini_CargaSiniestrosAPartirDeUnaMediaExternaASL(){}
+
+    public void OpeSini_BuscadorSiniestrosPendientes(){}
+
+    public void OpeSini_ActividadesPendientesReclamo(){}
+
+    public void OpeSini_HistorialActividadesReclamo(){}
+
+    public void OpeSini_HistorialReclamo(){}
+
+    public void OpeSini_ArchivosProcesadosPorASLClaim(){}
+
+    public void OpeSini_SiniestrosPorProcesar(){}
+
+    public void OpeSini_BusquedaDocumentos(){}
+
+    public void OpeSini_LiquidaciónSiniestro(){}
+
+    public void OpeSini_DeclaracionCartaGarantia(){}
+
+    public void OpeSini_AuditarCartaGarantia(){}
+
+    public void OpeSini_SiniestrosLiquidadosPorUsuarios(){}
+
+    public void OpeSini_ConsultaOrdenesPago(){}
+
+    public void OpeSini_GeneraciónPlanillasLiquidaciónPagos(){}
+
+
+
+
+    /** Coaseguro Reaseguro **/
+
+    public void CoaRea_OperaciónConsultaReaseguros(){}
+
+    public void CoaRea_MantenimientoDistribuciónReaseguro(){}
+
+    public void CoaRea_ContratoFacultativo(){}
+
+        /** CoaRea_CuentasTécnicas **/
+        public void CoaRea_CuentasTécnicas_LiquidaciónCuentaTécnica(){}
+        public void CoaRea_CuentasTécnicas_ConsultaCuentaTécnica(){}
+        public void CoaRea_CuentasTécnicas_PrevisualizacionCuentasTecnicas(){}
+        /** **/
+
+    public void CoaRea_LiquidaciónCoasegurador(){}
+
+    public void CoaRea_LiquidaciónReaseguro(){}
+
+    public void CoaRea_UtilidadesReaseguro(){}
+
+    public void CoaRea_BúsquedaLiquidacionReaseguro(){}
+
+    public void CoaRea_ConsultaCoaseguros(){}
+
+    public void CoaRea_SiniestrosCoaseguroContado(){}
+
+    public void CoaRea_HistúricoReaseguro(){}
+
+    public void CoaRea_ConsultaReaseguroPolizaMaestra(){}
+
+    public void CoaRea_PrimasDepositoReaseguro(){}
+
+    public void CoaRea_ConsultaSiniestrosContratosNoProporcionales(){}
+
+    public void CoaRea_ReinstalaciónLímiteAgregadoAnual(){}
+
+    public void CoaRea_AjustePrimasReaseguro(){}
+
+    public void CoaRea_FraccionamientoReaseguradoresFacultativos(){}
+
+
+
+
+    /** UAA (Administrador de Cuentas Universal) **/
+
+        /** Caja **/
+        public void UAA_Caja_AperturaCaja(){}
+
+        public void UAA_Caja_InicioSesión(){}
+
+        public void UAA_Caja_AplicaciónCobro(){}
+
+        public void UAA_Caja_ConsultaOperacionesCobranza(){}
+
+        public void UAA_Caja_ReversoRecaudación(){}
+
+        public void UAA_Caja_AplicaciónPagoSobreIngresosNoImputados(){}
+
+        public void UAA_Caja_Reportes(){}
+
+        public void UAA_Caja_SaldoDiarioPorCajero(){}
+
+        public void UAA_Caja_ReAperturaCaja(){}
+
+        public void UAA_Caja_CierreSesión(){}
+
+        public void UAA_Caja_CierreVariosCajeros(){}
+
+        public void UAA_Caja_CierreCaja(){}
+
+        public void UAA_Caja_PrimasPagadasACancelar(){}
+
+        public void UAA_Caja_PrimasPendientesACancelar(){}
+
+        public void UAA_Caja_PrimasVencidasNoPagadasYFondosInversión(){}
+
+        public void UAA_Caja_ConsultaFacturasGeneradas(){}
+
+        /** Caja **/
+
+    public void UAA_FacturaciónCobro(){}
+
+    public void UAA_ModificaciónMovimientosTercero(){}
+
+    public void UAA_Reportes(){}
+
+    public void UAA_EstadosCuenta(){}
+
+    public void UAA_Agentes(){}
+
+    public void UAA_LiquidaciónTercero(){}
+
+    public void UAA_ExportarMovimientosSAP(){}
+
+    public void UAA_DevoluciúnPrimaDepúsito(){}
+
+    public void UAA_NotificaciónVencimiento(){}
+
+    public void UAA_ReconciliaciónPagoPrima(){}
+
+    public void UAA_ConsultaMultimoneda(){}
+
+    public void UAA_AgrupacionAsientosContables(){}
+
+}
