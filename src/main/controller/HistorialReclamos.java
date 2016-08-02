7930ccdeda8a93535643864e87ff266895295d91
Adriana Azuaje <aazuaje@consis.local>
2016-08-02 17:38:06 -0430

    Creación de Historial de Reclamos

---
+++ src/main/controller/HistorialReclamos.java
@@ -0,0 +1,69 @@
+package main.controller;
+
+import beans.HistorialReclamosBean;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.io.IOException;
+
+/**
+ * Created by aazuaje on 02/08/2016.
+ */
+public class HistorialReclamos {
+
+    private final static Logger log = Logger.getLogger(HistorialReclamos.class);
+
+    public String nombreAutomatizacion = "Historial Reclamos";
+
+    public void testLink(HistorialReclamosBean historialReclamosBean,int i) throws IOException, InterruptedException {
+
+//implementando clase de metodos
+        Metodos a = new Metodos();
+        WebDriver driver = a.entrarPagina();
+        a.IniciarSesion(driver, nombreAutomatizacion);
+        a.ValidandoSesion(driver, nombreAutomatizacion);
+        Thread.sleep(5000);
+
+        MenuHistorialReclamos(driver, a);
+        Thread.sleep(8000);
+        a.cambiarVentana(driver);
+        BuscarHistorialReclamos(driver, a, historialReclamosBean);
+
+
+    }
+
+    public void MenuHistorialReclamos (WebDriver driver, Metodos a) throws InterruptedException, IOException {
+        WebElement menu1 = driver.findElement(By.xpath("/html/body/div[3]/div[2]"));//operacion
+        WebElement menu2 = driver.findElement(By.xpath("/html/body/div[5]/div[3]"));//operaciones siniestros
+        WebElement menu3 = driver.findElement(By.xpath("/html/body/div[10]/div[13]"));//historial reclamos
+
+        menu1.click();
+        menu2.click();
+        Thread.sleep(3000);
+        a.ScreenShot(driver,"screen3",nombreAutomatizacion);
+        menu3.click();
+    }
+
+    public void BuscarHistorialReclamos (WebDriver driver, Metodos a, HistorialReclamosBean historialReclamosBean) throws IOException, InterruptedException {
+        WebElement numeroReclamo = driver.findElement(By.xpath("//*[@id=\"tableHeader\"]/tbody/tr[1]/td[2]/input"));
+        numeroReclamo.sendKeys(historialReclamosBean.getNumeroReclamo());
+
+        Thread.sleep(2000);
+        a.ScreenShot(driver,"screen4",nombreAutomatizacion);
+
+        WebElement btnBuscar = driver.findElement(By.xpath("//*[@id=\"idb_0402006_claimHistoryScrem_01\"]"));
+        btnBuscar.click();
+
+        Thread.sleep(4000);
+        a.ScreenShot(driver,"screen5",nombreAutomatizacion);
+
+
+
+    }
+}
+
+
+
+
---
+++ src/tests/VistaHistorialReclamosTest.java
@@ -0,0 +1,60 @@
+package tests;
+
+import beans.HistorialReclamosBean;
+import main.controller.HistorialReclamos;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import util.DataSetManager;
+
+import java.util.ArrayList;
+/**
+ * Created by aazuaje on 02/08/2016.
+ */
+public class VistaHistorialReclamosTest {
+    private final static Logger log = Logger.getLogger(VistaHistorialReclamosTest.class);
+
+    @Test
+    public void mainTest() {
+
+        ArrayList historialReclamos = HistorialReclamosBean.getHistorialReclamos();
+
+        for (int i = 0; i < historialReclamos.size(); i++) {
+            HistorialReclamosBean historialReclamosBean = (HistorialReclamosBean) historialReclamos.get(i);
+            HistorialReclamos a = new HistorialReclamos();
+
+            try {
+                  a.testLink(historialReclamosBean,i);
+            } catch (Exception e) {
+                e.printStackTrace();
+//                log.info(e);
+                log.info("Test Case - " + a.nombreAutomatizacion + " - " + e);
+            }
+        }
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+        /*DataSetManager.createPartialDataSet("select Prueba, Numero_Reclamo from HIST_RECLAMOS ORDER BY PRUEBA ASC",
+                "HIST_RECLAMOS",
+                "C:/AcseleTests/AutomationTestAcsele/src/tests/historialReclamos_dataset.xml");
+
+        DataSetManager.createPartialDataSet("select Prueba,Numero_Reclamo from HIST_RECLAMOS ORDER BY PRUEBA ASC",
+                "HIST_RECLAMOS",
+                "C:/AcseleTests/AutomationTestAcsele/target/classes/tests/historialReclamos_dataset.xml");
+*/
+        DataSetManager.loadDataSet("/tests/historialReclamos_dataset.xml", DataSetManager.REFRESH_OPERATION);
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+
+        DataSetManager.loadDataSet("/tests/historialReclamos_dataset.xml", DataSetManager.DELETE_OPERATION);
+    }
+}
+
+
