cbfbf2f0f4b810a3c5523adfd9384eec63409c4f
Keizam <keila.zambrano.j@gmail.com>
2016-08-18 12:41:13 -0430

    Finalizacion de automatizacion Exportar e Importar Productos

--- src/AcseleV13_8/main/controller/ExportarImportarProducto.java
+++ src/AcseleV13_8/main/controller/ExportarImportarProducto.java
@@ -34,14 +34,18 @@ public class ExportarImportarProducto {
         Thread.sleep(2000);
         a.cambiarVentana(driver);
 
-        BuscarSiniestro (driver, a, exportarImportarProductoBean);
+        ExportarProducto(driver, a, exportarImportarProductoBean);
 
         Thread.sleep(3000);
+
+        ImportarProducto (driver, a,exportarImportarProductoBean);
+
+
 
 
     }
 
-    public void BuscarSiniestro (WebDriver driver, Metodos a, ExportarImportarProductoBean exportarImportarProductoBean) throws IOException, InterruptedException{
+    public void ExportarProducto (WebDriver driver, Metodos a, ExportarImportarProductoBean exportarImportarProductoBean) throws IOException, InterruptedException{
 
         try {
 
@@ -68,8 +72,12 @@ public class ExportarImportarProducto {
             exportarConf.click();
             Thread.sleep(1000);
             a.ScreenShot(driver,"screen6",nombreAutomatizacion);
-            Thread.sleep(3000);
+            Thread.sleep(4000);
             a.ScreenShot(driver,"screen7", nombreAutomatizacion);
+            Thread.sleep(2000);
+            WebElement cerrarExport = driver.findElement(By.xpath("/html/body/div[9]/div[1]/div/div/div/div"));
+            cerrarExport.click();
+
 
 
         }catch (Exception e) {
@@ -77,10 +85,81 @@ public class ExportarImportarProducto {
 //                log.info(e);
             log.info("Test Case - " + nombreAutomatizacion + " - " + e);
 
-
+         }
 
     }
 
-}
+    public void ImportarProducto (WebDriver driver, Metodos a, ExportarImportarProductoBean exportarImportarProductoBean) throws IOException, InterruptedException{
+
+        try {
+
+            Thread.sleep(2000);
+            WebElement btnMigracion = driver.findElement(By.xpath("//*[@id=\"ext-gen146\"]"));
+            btnMigracion.click();
+            Thread.sleep(1000);
+            a.ScreenShot(driver,"screen8",nombreAutomatizacion);
+            Thread.sleep(2000);
+
+
+            WebElement importarConf = driver.findElement(By.xpath("/html/body/div[5]/ul/li[9]/a"));
+            importarConf.click();
+            Thread.sleep(1000);
+            a.ScreenShot(driver,"screen9",nombreAutomatizacion);
+            Thread.sleep(2000);
+
+        /*    WebElement ruta = driver.findElement(By.xpath("/html/body/div[10]/div[2]/div[1]/div/div/div/div/div/div/div[1]/div/form/table/tbody[2]/tr[2]/td[1]/label"));
+
+            ruta.click();
+            Thread.sleep(1000);
+
+*/
+            WebElement archivo = driver.findElement(By.xpath("//*[@id=\"isc_7\"]"));
+            Thread.sleep(1000);
+      //      archivo.sendKeys("\\Qa32\\c$\\AcseleTests\\Export\\CO-ExportedProductTool-VidaDeudoresAvVillas-20160818094215PRUEBA.xml");
+            archivo.sendKeys("//Qa32//c$//AcseleTests//Export//CO-ExportedProductTool-VidaDeudoresAvVillas-20160818094215PRUEBA.xml");
+            Thread.sleep(2000);
+            a.ScreenShot(driver, "screen10", nombreAutomatizacion);
+            Thread.sleep(1000);
+
+
+
+            WebElement btncargar = driver.findElement(By.xpath("/html/body/div[10]/div[2]/div[1]/div/div/div/div/div/div/div[2]/div/table/tbody/tr/td"));
+            btncargar.click();
+            Thread.sleep(8000);
+            a.ScreenShot(driver, "screen11", nombreAutomatizacion);
+            Thread.sleep(1000);
+
+            WebElement nombre = driver.findElement(By.xpath("/html/body/center/table[2]/tbody/tr[7]/td/b/a"));
+            nombre.click();
+            Thread.sleep(1000);
+            a.ScreenShot(driver, "screen12", nombreAutomatizacion);
+            Thread.sleep(2000);
+
+            WebElement btnX = driver.findElement(By.xpath("//*[@id=\"resizablepanel\"]/a"));
+            btnX.click();
+            Thread.sleep(1000);
+
+            WebElement btnCerrar = driver.findElement(By.xpath("//*[@id=\"idb_04020140104_result_01\"]"));
+            Thread.sleep(1000);
+            btnCerrar.click();
+            Thread.sleep(2000);
+            a.ScreenShot(driver, "screen13", nombreAutomatizacion);
+
+
+
+
+
+
+
+
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+//                log.info(e);
+            log.info("Test Case - " + nombreAutomatizacion + " - " + e);
+
+        }
+    }
 
 }
