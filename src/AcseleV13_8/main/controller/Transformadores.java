381ad29f2738822fd4f9893d10ba8c7eb4f42f69
Keizam <keila.zambrano.j@gmail.com>
2016-08-22 15:29:25 -0430

    se suben cambios realizados, agregar y eliminar transformadores

--- src/AcseleV13_8/main/controller/Transformadores.java
+++ src/AcseleV13_8/main/controller/Transformadores.java
@@ -37,17 +37,23 @@ public class Transformadores {
         TranformadoresGeneral(driver, a, transformadoresBean);
         Thread.sleep(3000);
 
+        AgregarTranformadores( driver, a, transformadoresBean);
+        Thread.sleep(3000);
+
+        EliminarTransformadores (driver, a, transformadoresBean);
+
+
     }
 
     public void TranformadoresGeneral (WebDriver driver, Metodos a, TransformadoresBean transformadoresBean) throws IOException, InterruptedException{
 
         try {
 
-            Thread.sleep(4000);
+            Thread.sleep(7000);
             a.ScreenShot(driver, "screen4", nombreAutomatizacion);
             Thread.sleep(1000);
 
-       /*     WebElement buscar = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/div/div[3]/div/div/div[1]/div/div/div[2]/input"));
+           WebElement buscar = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/div/div[3]/div/div/div[1]/div/div/div[2]/input"));
             buscar.sendKeys(transformadoresBean.getBuscar());
             Thread.sleep(1000);
             a.ScreenShot(driver, "screen5", nombreAutomatizacion);
@@ -60,15 +66,15 @@ public class Transformadores {
             a.ScreenShot(driver, "screen6", nombreAutomatizacion);
 
 
-*/
-            WebElement carpetaTodas = driver.findElement(By.xpath("//*[@id=\"WControllerfront-1437825718\"]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/div/div[3]/div/div/div[2]/div/div[2]/div[1]/table/tbody/tr[1]/td/div/span"));
+
+  /*          WebElement carpetaTodas = driver.findElement(By.xpath("//*[@id=\"WControllerfront-1437825718\"]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/div/div[3]/div/div/div[2]/div/div[2]/div[1]/table/tbody/tr[1]/td/div/span"));
             Thread.sleep(1000);
             carpetaTodas.click();
             Thread.sleep(2500);
 
-
-            WebElement propiedad = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/div/div[3]/div/div/div[2]/div/div[2]/div[1]/table/tbody/tr[39]/td/div"));
-            Thread.sleep(2500);
+*/
+            Thread.sleep(3000);
+            WebElement propiedad = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/div/div[3]/div/div/div[2]/div/div[2]/div[1]/table/tbody/tr[59]/td/div"));
             propiedad.click();
             Thread.sleep(3000);
             a.ScreenShot(driver, "screen7", nombreAutomatizacion);
@@ -82,9 +88,43 @@ public class Transformadores {
 
 
 
+        }catch (Exception e) {
+            e.printStackTrace();
+//                log.info(e);
+            log.info("Test Case - " + nombreAutomatizacion + " - " + e);
 
+        }
+    }
 
+    public void AgregarTranformadores (WebDriver driver, Metodos a, TransformadoresBean transformadoresBean) throws IOException, InterruptedException{
 
+        try {
+
+            WebElement valorDefecto = driver.findElement(By.xpath("/html/body/div[5]/div[3]/div/div/div[5]/div/div/div[4]/div/div[3]/input"));
+            valorDefecto.sendKeys(transformadoresBean.getValorDefecto());
+            Thread.sleep(1000);
+            a.ScreenShot(driver, "screen9", nombreAutomatizacion);
+            Thread.sleep(1000);
+
+            WebElement valorPropiedad = driver.findElement(By.xpath("/html/body/div[5]/div[3]/div/div/div[5]/div/div/div[4]/div/div[9]/input"));
+            valorPropiedad.sendKeys(transformadoresBean.getValorPropiedad());
+            Thread.sleep(1000);
+            a.ScreenShot(driver, "screen10", nombreAutomatizacion);
+            Thread.sleep(2000);
+
+            WebElement btnAgregar = driver.findElement(By.xpath("/html/body/div[5]/div[3]/div/div/div[5]/div/div/div[2]/div/div[1]/button"));
+            Thread.sleep(1000);
+            btnAgregar.click();
+            Thread.sleep(1000);
+            a.ScreenShot(driver, "screen11", nombreAutomatizacion);
+            Thread.sleep(1500);
+
+            WebElement btnAceptar = driver.findElement(By.xpath("/html/body/div[5]/div[3]/div/div/div[5]/div/div/div[5]/div/div[1]/button"));
+        //    Thread.sleep(1000);
+            btnAceptar.click();
+            Thread.sleep(8000);
+            a.ScreenShot(driver, "screen12", nombreAutomatizacion);
+            Thread.sleep(2000);
 
 
 
@@ -92,8 +132,65 @@ public class Transformadores {
             e.printStackTrace();
 //                log.info(e);
             log.info("Test Case - " + nombreAutomatizacion + " - " + e);
-
         }
+
+
+    }
+
+    public void EliminarTransformadores (WebDriver driver, Metodos a, TransformadoresBean transformadoresBean) throws IOException, InterruptedException{
+
+        try {
+
+         /*   WebElement carpetaTodas = driver.findElement(By.xpath("//*[@id=\"WControllerfront-1437825718\"]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/div/div[3]/div/div/div[2]/div/div[2]/div[1]/table/tbody/tr[1]/td/div/span"));
+            Thread.sleep(1000);
+            carpetaTodas.click();
+            Thread.sleep(2500);
+         */
+
+
+            Thread.sleep(3000);
+            WebElement opcionTranformador = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div[3]/div/div/div/div/div[1]/button"));
+            Thread.sleep(1000);
+            opcionTranformador.click();
+            Thread.sleep(2000);
+            a.ScreenShot(driver, "screen13", nombreAutomatizacion);
+
+            WebElement selectTransf = driver.findElement(By.xpath("/html/body/div[5]/div[3]/div/div/div[5]/div/div/div[3]/div/div[2]/div[2]/div[1]/table/tbody/tr/td[1]/div"));
+            Thread.sleep(1000);
+            selectTransf.click();
+            Thread.sleep(1000);
+            a.ScreenShot(driver, "screen14", nombreAutomatizacion);
+
+
+            WebElement btnEliminar = driver.findElement(By.xpath("/html/body/div[5]/div[3]/div/div/div[5]/div/div/div[2]/div/div[3]/button"));
+            Thread.sleep(1000);
+            btnEliminar.click();
+            Thread.sleep(2000);
+            a.ScreenShot(driver, "screen15", nombreAutomatizacion);
+            Thread.sleep(1500);
+
+            WebElement btnAceptar = driver.findElement(By.xpath("/html/body/div[5]/div[3]/div/div/div[5]/div/div/div[5]/div/div[1]/button"));
+            Thread.sleep(1000);
+            btnAceptar.click();
+            Thread.sleep(7000);
+            a.ScreenShot(driver, "screen16", nombreAutomatizacion);
+
+
+            Thread.sleep(2000);
+            WebElement opcionTranf = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div[3]/div/div/div/div/div[1]/button"));
+            Thread.sleep(1000);
+            opcionTranf.click();
+            Thread.sleep(2000);
+            a.ScreenShot(driver, "screen17", nombreAutomatizacion);
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+//                log.info(e);
+            log.info("Test Case - " + nombreAutomatizacion + " - " + e);
+        }
+
+
     }
 
 }
