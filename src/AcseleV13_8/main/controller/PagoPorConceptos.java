33c44b2669d11d2556675cf1b767b8c34ff75905
Keizam <keila.zambrano.j@gmail.com>
2016-09-13 17:40:00 -0430

    Se sube el avance de la nueva funcionalidad: Pago Por conceptos

---
+++ src/AcseleV13_8/main/controller/PagoPorConceptos.java
@@ -0,0 +1,22 @@
+package AcseleV13_8.main.controller;
+
+import AcseleV13_8.beans.PagoPorConceptosBean;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+/**
+ * Created by kzambrano on 13/09/2016.
+ */
+public class PagoPorConceptos {
+
+    private final static Logger log = Logger.getLogger(AprobarEnviarPagoSiniestros.class);
+
+    public String nombreAutomatizacion = "Pago Por Conceptos ";
+
+    public void testLink (PagoPorConceptosBean pagoPorConceptosBean, int i )  throws IOException, InterruptedException {
+
+    }
+
+
+}
