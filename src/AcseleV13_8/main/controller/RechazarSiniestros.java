acbbb20e55ef058b42ff16ac95a2668d288c0120
Adriana Azuaje <aazuaje@consis.local>
2016-09-22 17:47:33 -0430

    Culminacion modulo Rechazar Siniestro.

---
+++ src/AcseleV13_8/main/controller/RechazarSiniestros.java
@@ -0,0 +1,209 @@
+package AcseleV13_8.main.controller;
+
+import AcseleV13_8.beans.RechazarSiniestroBean;
+import AcseleV13_8.main.controller.Menu.MenuOperaciones;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by aazuaje on 22/09/2016.
+ */
+public class RechazarSiniestros {
+
+    private final static Logger log = Logger.getLogger(CerrarSiniestros.class);
+
+    public String nombreAutomatizacion = "Rechazar Siniestro";
+
+    public void testLink(RechazarSiniestroBean rechazarSiniestroBean,int i) throws IOException, InterruptedException {
+
+        //implementando clase de metodos
+        Metodos a = new Metodos();
+        MenuOperaciones m = new MenuOperaciones();
+        WebDriver driver = a.entrarPagina();
+        a.IniciarSesion(driver, nombreAutomatizacion, i);
+        a.ValidandoSesion(driver, nombreAutomatizacion, i);
+        Thread.sleep(5000);
+
+        m.OpeSini_MantenimientoSiniestro(a, driver, nombreAutomatizacion, i);
+        Thread.sleep(10000);
+
+        a.cambiarVentana(driver);
+        BuscarPoliza(driver, a, rechazarSiniestroBean, i);
+        ResultadoBusqueda(driver, a, rechazarSiniestroBean, i);
+        RechazarSiniestro (driver, a, rechazarSiniestroBean, i);
+
+
+
+    }
+
+    public void BuscarPoliza(WebDriver driver, Metodos a, RechazarSiniestroBean rechazarSiniestroBean, int i) throws IOException, InterruptedException{
+
+        try {
+
+            if (rechazarSiniestroBean.getOrdenarPor() != null){
+                Thread.sleep(1000);
+
+                if (rechazarSiniestroBean.getOrdenarPor() == "Número de Reclamo"){
+                    Thread.sleep(1000);
+                    WebElement btnSeleccionar1 = driver.findElement(By.xpath("//*[@id=\"orderBy\"]/div"));
+                    btnSeleccionar1.click();
+                    Thread.sleep(1000);
+                    WebElement ordenarPor  = driver.findElement(By.xpath("//*[@id=\"VAADIN_COMBOBOX_OPTIONLIST\"]/div/div[2]/table/tbody/tr[2]/td/span"));
+                    ordenarPor.click();
+                }
+
+                if (rechazarSiniestroBean.getOrdenarPor() == "Producto"){
+                    Thread.sleep(1000);
+                    WebElement btnSeleccionar1 = driver.findElement(By.xpath("//*[@id=\"orderBy\"]/div"));
+                    btnSeleccionar1.click();
+                    Thread.sleep(1000);
+                    WebElement ordenarPor  = driver.findElement(By.xpath("//*[@id=\"VAADIN_COMBOBOX_OPTIONLIST\"]/div/div[2]/table/tbody/tr[3]/td"));
+                    ordenarPor.click();
+                }
+
+                if (rechazarSiniestroBean.getOrdenarPor() == "Fecha de Ocurrencia"){
+                    Thread.sleep(1000);
+                    WebElement btnSeleccionar1 = driver.findElement(By.xpath("//*[@id=\"orderBy\"]/div"));
+                    btnSeleccionar1.click();
+                    Thread.sleep(1000);
+                    WebElement ordenarPor  = driver.findElement(By.xpath("//*[@id=\"VAADIN_COMBOBOX_OPTIONLIST\"]/div/div[2]/table/tbody/tr[4]/td/span"));
+                    ordenarPor.click();
+                }
+
+                if (rechazarSiniestroBean.getOrdenarPor() == "Estado del siniestro"){
+                    Thread.sleep(1000);
+                    WebElement btnSeleccionar1 = driver.findElement(By.xpath("//*[@id=\"orderBy\"]/div"));
+                    btnSeleccionar1.click();
+                    Thread.sleep(1000);
+                    WebElement ordenarPor  = driver.findElement(By.xpath("//*[@id=\"VAADIN_COMBOBOX_OPTIONLIST\"]/div/div[2]/table/tbody/tr[5]/td/span"));
+                    ordenarPor.click();
+                }
+
+            }
+
+            if (rechazarSiniestroBean.getNumSiniestro() != null){
+                Thread.sleep(2000);
+                WebElement numSiniestro = driver.findElement(By.xpath("//*[@id=\"textFieldClaimNumbre\"]"));
+                numSiniestro.sendKeys(rechazarSiniestroBean.getNumSiniestro());
+            }
+
+            if (rechazarSiniestroBean.getNumPoliza() != null){
+                Thread.sleep(2000);
+                WebElement numPoliza = driver.findElement(By.xpath("//*[@id=\"textFieldPolicyNumber\"]"));
+                numPoliza.sendKeys(rechazarSiniestroBean.getNumPoliza());
+            }
+
+            if (rechazarSiniestroBean.getFechaOcurrenciaSiniestro() != null){
+                Thread.sleep(2000);
+                WebElement fechaOcurrenciaSiniestro  = driver.findElement(By.xpath("//*[@id=\"dateFieldOccurrenceDate\"]/button"));
+                fechaOcurrenciaSiniestro.sendKeys(rechazarSiniestroBean.getFechaOcurrenciaSiniestro());
+            }
+
+            String productos = "VidaDeudoresAvVillas";
+
+            if (rechazarSiniestroBean.getProducto() != null){
+                //if (editarSiniestrosBean.getProducto() == productos){
+                Thread.sleep(1000);
+                WebElement btnSeleccionar = driver.findElement(By.xpath("//*[@id=\"comboProductoSimpleSearch\"]/div"));
+                btnSeleccionar.click();
+                Thread.sleep(1000);
+                WebElement producto  = driver.findElement(By.xpath("//*[@id=\"VAADIN_COMBOBOX_OPTIONLIST\"]/div/div[2]/table/tbody/tr[2]/td/span"));
+                producto.click();
+                //}
+            }
+
+            Thread.sleep(1000);
+            a.ScreenShotPool(driver, i,"screen4",nombreAutomatizacion);
+
+            WebElement btnBuscar  = driver.findElement(By.xpath("//*[@id=\"buttonBuscar\"]/span"));
+            btnBuscar.click();
+
+            Thread.sleep(4000);
+
+        }catch (Exception e) {
+            e.printStackTrace();
+//                log.info(e);
+            log.info("Test Case - " + nombreAutomatizacion + " - " + e);
+        }
+    }
+
+    public void ResultadoBusqueda(WebDriver driver,Metodos a, RechazarSiniestroBean rechazarSiniestroBean, int i) throws IOException, InterruptedException{
+
+        try{
+            Thread.sleep(1000);
+            WebElement btnSeleccionarPoliza  = driver.findElement(By.xpath("//*[@id=\"layoutResultTable\"]/div[1]/div/div[3]/div[1]/table/tbody/tr[5]/td[3]/div"));
+            btnSeleccionarPoliza.click();
+
+            Thread.sleep(1000);
+            a.ScreenShotPool(driver,i,"screen5",nombreAutomatizacion);
+
+            WebElement btnConsultar = driver.findElement(By.xpath("//*[@id=\"buttonOk\"]/span/span"));
+            btnConsultar.click();
+
+            Thread.sleep(15000);
+            a.changeLastWindows(driver);
+
+        }catch (Exception e) {
+            e.printStackTrace();
+//                log.info(e);
+            log.info("Test Case - " + nombreAutomatizacion + " - " + e);
+        }
+    }
+
+    public void RechazarSiniestro (WebDriver driver,Metodos a, RechazarSiniestroBean rechazarSiniestroBean, int i) throws IOException, InterruptedException{
+
+        try {
+            Thread.sleep(2000);
+            WebElement btnObjetar = driver.findElement(By.xpath("//*[@id=\"idb_0402006_structure_29\"]"));
+            btnObjetar.click();
+
+            Thread.sleep(15000);
+
+            Select motivoCierre = new Select(driver.findElement(By.xpath("/html/body/div[14]/div[2]/div[2]/select")));
+            motivoCierre.selectByValue(rechazarSiniestroBean.getMotivoObjecion());
+            a.ScreenShotPool(driver,i,"screen6",nombreAutomatizacion);
+            Thread.sleep(2000);
+
+            // Cambiar de frame
+            driver.switchTo().frame("plantilla");
+
+            WebElement fechaObjecion = driver.findElement(By.xpath("//*[@id=\"28482594\"]/td[3]/font/input[1]"));
+            fechaObjecion.sendKeys(rechazarSiniestroBean.getFechaObjecion());
+
+            if (rechazarSiniestroBean.getNumSiniestro() != null){
+                Thread.sleep(2000);
+                WebElement comentarioSiniestro = driver.findElement(By.xpath("//*[@id=\"calendarFechaRechazo\"]"));
+                comentarioSiniestro.sendKeys(rechazarSiniestroBean.getComentarioSiniestro());
+            }
+
+
+            // Salir del frame
+            //driver.switchTo().parentFrame();
+            driver.switchTo().defaultContent();
+
+            Thread.sleep(3000);
+            WebElement btnAceptar2 = driver.findElement(By.xpath("/html/body/div[14]/div[2]/div[4]/input"));
+            btnAceptar2.click();
+            Thread.sleep(3000);
+            a.ScreenShotPool(driver,i,"screen7",nombreAutomatizacion);
+
+
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+//                log.info(e);
+            log.info("Test Case - " + nombreAutomatizacion + " - " + e);
+        }
+
+
+    }
+
+   }
