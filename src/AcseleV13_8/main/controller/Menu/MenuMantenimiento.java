4969fdd4d0ff9d32b3a6d4c576cac285f6cdae10
Adriana Azuaje <aazuaje@consis.local>
2016-10-24 17:51:35 -0430

    Cambio

--- src/AcseleV13_8/main/controller/Menu/MenuMantenimiento.java
+++ src/AcseleV13_8/main/controller/Menu/MenuMantenimiento.java
@@ -1,6 +1,7 @@
 package AcseleV13_8.main.controller.Menu;
 
 import AcseleV13_8.main.controller.Metodos;
+import AcseleV13_8LBC.main.controller.MetodosLBC;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -51,7 +52,7 @@ public class MenuMantenimiento {
             }
         }
 
-        public void MantTerc_BuscarTercero(Metodos a, WebDriver driver, String nombreAutomatizacion, int numScreenShot){
+        public void MantTerc_BuscarTercero(MetodosLBC a, WebDriver driver, String nombreAutomatizacion, int numScreenShot){
 
             try {
                 Actions action = new Actions(driver);
