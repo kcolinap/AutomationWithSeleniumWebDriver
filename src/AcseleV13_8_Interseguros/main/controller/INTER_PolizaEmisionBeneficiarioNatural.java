226dcfb98c4831b4d6ce205e0e0db1b78da1abcd
Adriana Azuaje <aazuaje@consis.local>
2016-12-06 11:53:31 -0430

    se culmina el modulo de emitir con beneficiario natural

---
+++ src/AcseleV13_8_Interseguros/main/controller/INTER_PolizaEmisionBeneficiarioNatural.java
@@ -0,0 +1,104 @@
+package AcseleV13_8_Interseguros.main.controller;
+
+import AcseleV13_8_Interseguros.beans.INTER_PolizaEmisionBeneficiarioNaturalBean;
+import AcseleV13_8_Interseguros.beans.Inter_PolizaEmisionVariosOABean;
+import AcseleV13_8_Interseguros.main.controller.Interseguros_Menu.Interseguros_MenuOperaciones;
+import AcseleV13_8_Interseguros.main.controller.PolizaEmision.*;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * Created by aazuaje on 02/12/2016.
+ */
+public class INTER_PolizaEmisionBeneficiarioNatural {
+
+    private final static Logger log = Logger.getLogger(INTER_PolizaEmisionBeneficiarioNatural.class);
+
+    public String nombreAutomatizacion = "INTER Poliza Emision Beneficiario Natural";
+
+    public void testLink(INTER_PolizaEmisionBeneficiarioNaturalBean inter_polizaEmisionBeneficiarioNaturalBean, int i) throws IOException, InterruptedException {
+
+        try {
+
+            // Instanciando clases
+            Interseguros_Metodos a = new Interseguros_Metodos();
+            Interseguros_MenuOperaciones intersegurosMenuOperaciones = new Interseguros_MenuOperaciones();
+            Inter_PrePoliza interPrePoliza = new Inter_PrePoliza();
+            Inter_InformacionGeneralPoliza interInformacionGeneralPoliza = new Inter_InformacionGeneralPoliza();
+            Inter_Contratante interContratante = new Inter_Contratante();
+            Inter_UnidadesRiesgo interUnidadesRiesgo = new Inter_UnidadesRiesgo();
+            Inter_ObjetosAsegurados interObjetosAsegurados = new Inter_ObjetosAsegurados();
+            Inter_Asegurado interAsegurado = new Inter_Asegurado();
+            Inter_Beneficiario interBeneficiario = new Inter_Beneficiario();
+            Inter_Calcular interCalcular = new Inter_Calcular();
+            Inter_Validar interValidar = new Inter_Validar();
+
+            WebDriver driver = a.entrarPagina();
+            a.IniciarSesion(driver, nombreAutomatizacion, i);
+            a.ValidandoSesion(driver, nombreAutomatizacion, i);
+            Thread.sleep(5000);
+
+
+            //Entrando en Menu
+            intersegurosMenuOperaciones.OpePol_Crear(driver, nombreAutomatizacion, 2, i);
+
+            Thread.sleep(2000);
+            a.cambiarVentana(driver);
+            Thread.sleep(2000);
+
+            /** Crear Poliza */
+
+            Thread.sleep(3000);
+            interPrePoliza.AdministracionPropuestaPoliza(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 3);
+            Thread.sleep(2000);
+            interPrePoliza.EvAplicar(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 4);
+            Thread.sleep(2000);
+            if (inter_polizaEmisionBeneficiarioNaturalBean.getProducto().equals("Inteligo")) {
+                interInformacionGeneralPoliza.InformacionGeneralInteligo(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 5);
+                Thread.sleep(2000);
+                interContratante.AgregarContratante(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 6, 7);
+                Thread.sleep(2000);
+                interUnidadesRiesgo.UnidadesRiesgo1(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 8);
+                Thread.sleep(2000);
+                interObjetosAsegurados.ObjetoAsegurado1_Inteligo(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 9);
+                Thread.sleep(2000);
+                interAsegurado.AgregarAsegurado1(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 10, 11);
+                Thread.sleep(2000);
+                interBeneficiario.AgregarBeneficiario1(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 12, 13);
+                Thread.sleep(2000);
+                //interObjetosAsegurados.ObjetoAsegurado2_Inteligo(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 14);
+                //Thread.sleep(2000);
+                interAsegurado.AgregarAsegurado2(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 15, 16);
+                Thread.sleep(2000);
+                interBeneficiario.AgregarBeneficiario2(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 17, 18);
+                Thread.sleep(2000);
+                interCalcular.Calcular(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 19, 20);
+                Thread.sleep(2000);
+                interValidar.ValidarEmisionPoliza(driver, nombreAutomatizacion);
+            }
+            else if (inter_polizaEmisionBeneficiarioNaturalBean.getProducto().equals("Inteligo")){
+                interInformacionGeneralPoliza.InformacionGeneralDotalSimple(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 5, 6, 7);
+                Thread.sleep(2000);
+                interContratante.AgregarContratante(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 8, 9);
+                Thread.sleep(2000);
+                interUnidadesRiesgo.UnidadesRiesgo1DotalSimple(a, driver, inter_polizaEmisionBeneficiarioNaturalBean, nombreAutomatizacion, i, 10);
+                Thread.sleep(2000);
+                //interObjetosAsegurados.ObjetoAsegurado1_DotalSimple(a, driver, interPolizaEmisionVariosOABean, nombreAutomatizacion, i, 9);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            sw.toString(); // stack trace as a string
+            //log.info("Test Case - " + nombreAutomatizacion + " - " + e);
+            log.info("Test Case - " + nombreAutomatizacion + " - " + sw.toString());
+        }
+    }
+
+}
--- src/AcseleV13_8_Interseguros/tests/VistaINTER_PolizaEmisionBeneficiarioNaturalTest.java
+++ src/AcseleV13_8_Interseguros/tests/VistaINTER_PolizaEmisionBeneficiarioNaturalTest.java
@@ -2,6 +2,7 @@ package AcseleV13_8_Interseguros.tests;
 
 
 import AcseleV13_8_Interseguros.beans.INTER_PolizaEmisionBeneficiarioNaturalBean;
+import AcseleV13_8_Interseguros.main.controller.INTER_PolizaEmisionBeneficiarioNatural;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
@@ -33,10 +34,10 @@ public class VistaINTER_PolizaEmisionBeneficiarioNaturalTest {
 
         for (int j = 0; j < polizaEmisionBeneficiarioNatural.size(); j++) {
             INTER_PolizaEmisionBeneficiarioNaturalBean inter_polizaEmisionBeneficiarioNaturalBean = (INTER_PolizaEmisionBeneficiarioNaturalBean) polizaEmisionBeneficiarioNatural.get(j);
-            //INTER_PolizaEmisionBeneficiarioNatural a = new INTER_PolizaEmisionBeneficiarioNatural();
+            INTER_PolizaEmisionBeneficiarioNatural a = new INTER_PolizaEmisionBeneficiarioNatural();
 
             try {
-                //a.testLink(inter_polizaEmisionBeneficiarioNaturalBean, j);
+                a.testLink(inter_polizaEmisionBeneficiarioNaturalBean, j);
             } catch (Exception e) {
                 //e.printStackTrace();
                 //log.error("Test Case - " + a.nombreAutomatizacion + " - " + e);
@@ -45,7 +46,7 @@ public class VistaINTER_PolizaEmisionBeneficiarioNaturalTest {
                 PrintWriter pw = new PrintWriter(sw);
                 e.printStackTrace(pw);
                 sw.toString(); // stack trace as a string
-                //log.info("Test Case - " + a.nombreAutomatizacion + " - " + sw.toString());
+                log.info("Test Case - " + a.nombreAutomatizacion + " - " + sw.toString());
             }
         }
 
@@ -54,14 +55,14 @@ public class VistaINTER_PolizaEmisionBeneficiarioNaturalTest {
     @Before
     public void setUp() throws Exception {
 
-        DataSetManager.createPartialDataSet("select * FROM INTER_EMISION_BENEF_NATUR ORDER BY PRUEBA ASC",
+        /*DataSetManager.createPartialDataSet("select * FROM INTER_EMISION_BENEF_NATUR ORDER BY PRUEBA ASC",
                 "INTER_EMISION_BENEF_NATUR",
                 "C:/AcseleTests/AutomationTestAcsele/src/AcseleV13_8_Interseguros/tests/xmls/inter_polizaEmisionBeneficiarioNatural_dataset.xml");
 
         DataSetManager.createPartialDataSet("select * FROM INTER_EMISION_BENEF_NATUR ORDER BY PRUEBA ASC",
                 "INTER_EMISION_BENEF_NATUR",
                 "C:/AcseleTests/AutomationTestAcsele/target/classes/AcseleV13_8_Interseguros/tests/xmls/inter_polizaEmisionBeneficiarioNatural_dataset.xml");
-
+*/
         DataSetManager.loadDataSet("/AcseleV13_8_Interseguros/tests/xmls/inter_polizaEmisionBeneficiarioNatural_dataset.xml", DataSetManager.REFRESH_OPERATION);
     }
 
@@ -70,5 +71,6 @@ public class VistaINTER_PolizaEmisionBeneficiarioNaturalTest {
 
         DataSetManager.loadDataSet("/AcseleV13_8_Interseguros/tests/xmls/inter_polizaEmisionBeneficiarioNatural_dataset.xml", DataSetManager.DELETE_OPERATION);
     }
+
 
 }
