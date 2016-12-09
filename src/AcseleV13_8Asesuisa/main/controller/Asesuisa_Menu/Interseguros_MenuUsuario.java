c4bd85d35f0d2bf410aeac9e2620164e30bf3fc6
Alejandro Gil <agil@consisint.com>
2016-12-09 14:47:42 -0430

    Paquetes y clases creadas

---
+++ src/AcseleV13_8Asesuisa/main/controller/Asesuisa_Menu/Interseguros_MenuUsuario.java
@@ -0,0 +1,12 @@
+package AcseleV13_8Asesuisa.main.controller.Asesuisa_Menu;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by agil on 25/07/2016.
+ */
+public class Interseguros_MenuUsuario {
+
+    protected ThreadLocal<WebDriver> threadDriver = null;
+
+}
