ac3488e44e5c17f997bf104a679165740b1e7cb5
Rene Montilla <rmontilla@consisint.com>
2016-12-26 10:28:11 -0430

    avance

---
+++ src/AcseleV13_8_Interseguros/main/controller/INTER_ConsultaSimplePoliza.java
@@ -0,0 +1,7 @@
+package AcseleV13_8_Interseguros.main.controller;
+
+/**
+ * Created by rmontilla on 26/12/2016.
+ */
+public class INTER_ConsultaSimplePoliza {
+}
---
+++ src/AcseleV13_8_Interseguros/tests/VistaINTER_ConsultaSimplePolizaTest.java
@@ -0,0 +1,7 @@
+package AcseleV13_8_Interseguros.tests;
+
+/**
+ * Created by rmontilla on 26/12/2016.
+ */
+public class VistaINTER_ConsultaSimplePolizaTest {
+}
