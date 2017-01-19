1014f001f5721ca5b4658f836ae3c2a836de1519
kelvin colina <kcolina@consisint.com>
2017-01-19 17:14:14 -0400

    Creacion del HEAD de la clase principal para funcionalidad: Edicion de apellido y nombre de terceros

---
+++ src/AcseleV13_8_Interseguros/main/controller/INTER_TercerosEditarNombreApellido.java
@@ -0,0 +1,24 @@
+package AcseleV13_8_Interseguros.main.controller;
+
+import AcseleV13_8_Interseguros.beans.INTER_TercerosEditarNombreApellidoBean;
+import AcseleV13_8_Interseguros.main.controller.Interseguros_Menu.Interseguros_MenuMantenimiento;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+
+/**
+ * Created by kcolina on 19/01/2017.
+ */
+public class INTER_TercerosEditarNombreApellido {
+
+    private final static Logger Log = Logger.getLogger(INTER_TercerosEditarNombreApellido.class);
+
+    public String nombreAutomatizacion = "Edicion de nombre y apellido de terceros";
+
+    public void textLink(INTER_TercerosEditarNombreApellidoBean inter_tercerosEditarNombreApellidoBean, int i) throws Exception{
+
+    }
+}
