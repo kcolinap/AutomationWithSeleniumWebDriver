a5bfe4a95b62ceda15d50d876b6151fbea73eea1
kelvin colina <kcolina@consisint.com>
2017-01-20 16:00:24 -0400

    Subida de avances de funcionalidad: Editar nombre y apellido de terceros

--- src/AcseleV13_8_Interseguros/beans/INTER_TercerosEditarNombreApellidoBean.java
+++ src/AcseleV13_8_Interseguros/beans/INTER_TercerosEditarNombreApellidoBean.java
@@ -18,6 +18,7 @@ public class INTER_TercerosEditarNombreApellidoBean implements Serializable {
     private final static Logger log = Logger.getLogger(INTER_TercerosEditarNombreApellidoBean.class);
 
     private String tipoTerceros;
+    private String numDocIdentificacion;
     private String apellido;
     private String nombre;
 
@@ -45,6 +46,14 @@ public class INTER_TercerosEditarNombreApellidoBean implements Serializable {
         this.nombre = nombre;
     }
 
+    public String getNumDocIdentificacion() {
+        return numDocIdentificacion;
+    }
+
+    public void setNumDocIdentificacion(String numDocIdentificacion) {
+        this.numDocIdentificacion = numDocIdentificacion;
+    }
+
     public static ArrayList getINTER_TercerosEditarNombreApellido() throws SQLException {
 
         Connection conn = null;
@@ -53,7 +62,7 @@ public class INTER_TercerosEditarNombreApellidoBean implements Serializable {
         ArrayList interTercerosEditarNombreApellido = new ArrayList();
 
         StringBuilder queryLoad = new StringBuilder();
-        queryLoad.append("SELECT * FROM INTER_TERCEROS_EDITAR_NOMBRE_APELLIDO ORDER BY Prueba ASC");
+        queryLoad.append("SELECT * FROM INTER_TERCEROS_EDITAR_NOM_APE ORDER BY Prueba ASC");
 
         try{
 
@@ -66,6 +75,7 @@ public class INTER_TercerosEditarNombreApellidoBean implements Serializable {
                 INTER_TercerosEditarNombreApellidoBean inter_TercerosEditarNombreApellidoBean = new INTER_TercerosEditarNombreApellidoBean();
 
                 inter_TercerosEditarNombreApellidoBean.setTipoTerceros(rs.getString("Tipo_Tercero"));
+                inter_TercerosEditarNombreApellidoBean.setNumDocIdentificacion(rs.getString("Num_Doc_Identificacion"));
                 inter_TercerosEditarNombreApellidoBean.setApellido(rs.getString("Apellido"));
                 inter_TercerosEditarNombreApellidoBean.setNombre(rs.getString("Nombre"));
 
--- src/AcseleV13_8_Interseguros/main/controller/INTER_TercerosEditarNombreApellido.java
+++ src/AcseleV13_8_Interseguros/main/controller/INTER_TercerosEditarNombreApellido.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+import java.awt.*;
+import java.io.IOException;
+
 
 /**
  * Created by kcolina on 19/01/2017.
@@ -17,15 +20,98 @@ public class INTER_TercerosEditarNombreApellido {
     private final static Logger Log = Logger.getLogger(INTER_TercerosEditarNombreApellido.class);
 
     public String nombreAutomatizacion = "Edicion de nombre y apellido de terceros";
+    private WebDriver driver;
 
-    public void textLink(INTER_TercerosEditarNombreApellidoBean inter_tercerosEditarNombreApellidoBean, int i) throws Exception{
+    public void testLink(INTER_TercerosEditarNombreApellidoBean inter_tercerosEditarNombreApellidoBean, int i) throws Exception{
+        try{
 
-        Interseguros_Metodos a = new Interseguros_Metodos();
-        Interseguros_MenuMantenimiento intersegurosMenuMantenimiento = new Interseguros_MenuMantenimiento();
+            Interseguros_Metodos a = new Interseguros_Metodos();
+            Interseguros_MenuMantenimiento intersegurosMenuMantenimiento = new Interseguros_MenuMantenimiento();
 
-        WebDriver driver = a.entrarPagina();
-        a.IniciarSesion(driver, nombreAutomatizacion, i);
-        a.ValidandoSesion(driver, nombreAutomatizacion, i);
-        Thread.sleep(5000);
+            WebDriver driver = a.entrarPagina();
+            a.IniciarSesion(driver, nombreAutomatizacion, i);
+            a.ValidandoSesion(driver, nombreAutomatizacion, i);
+            Thread.sleep(5000);
+
+            //Ingreso a la opcion Consulta de terceros
+            intersegurosMenuMantenimiento.MantTerc_BuscarTercero(driver, nombreAutomatizacion, i, 2);
+
+            Thread.sleep(2000);
+            a.cambiarVentana(driver);
+            Thread.sleep(2000);
+            BusquedaT(a, driver, inter_tercerosEditarNombreApellidoBean, i);
+
+            //Boton editar
+            WebElement editar = driver.findElement(By.xpath("//input[@wicketpath='SearchContent_ThirdInformation_showDetailSearchTable_proof_TableForm_associateButton']"));
+            editar.click();
+            Thread.sleep(4000);
+
+            driver.quit();
+        }catch (Exception e){
+            e.printStackTrace();
+            Log.error("Test Case - " + nombreAutomatizacion + " - " + e);
+            if(driver != null){
+                driver.quit();
+            }
+        }
+    }
+
+    public void BusquedaT(Interseguros_Metodos a, WebDriver driver, INTER_TercerosEditarNombreApellidoBean inter_tercerosEditarNombreApellidoBean, int i) throws InterruptedException, IOException{
+
+        try{
+
+            Thread.sleep(4000);
+
+            //Tipo de tercero
+            if(inter_tercerosEditarNombreApellidoBean.getTipoTerceros() != null){
+                Thread.sleep(3000);
+                Select tipoTerceros = new Select(driver.findElement(By.xpath("//select[@wicketpath='SearchContent_ThirdInformation_thirdPartyTypes']")));
+                tipoTerceros.selectByValue(inter_tercerosEditarNombreApellidoBean.getTipoTerceros());
+            }
+
+            Thread.sleep(2000);
+
+            if(inter_tercerosEditarNombreApellidoBean.getNumDocIdentificacion() != null){
+                WebElement nroDocIden = driver.findElement(By.xpath("//input[@wicketpath='SearchContent_ThirdInformation_templateContainer_searchForm_templateThird_repeaterPanel1_1_fila_field']"));
+                nroDocIden.sendKeys(inter_tercerosEditarNombreApellidoBean.getNumDocIdentificacion());
+            }
+
+            Thread.sleep(2000);
+
+            if(inter_tercerosEditarNombreApellidoBean.getApellido() != null){
+                WebElement apellido = driver.findElement(By.xpath("//input[@wicketpath='SearchContent_ThirdInformation_templateContainer_searchForm_templateThird_repeaterPanel1_7_fila_field']"));
+                apellido.sendKeys(inter_tercerosEditarNombreApellidoBean.getApellido());
+            }
+
+            Thread.sleep(2000);
+
+            if(inter_tercerosEditarNombreApellidoBean.getNombre() != null){
+                WebElement nombre = driver.findElement(By.xpath("//input[@wicketpath='SearchContent_ThirdInformation_templateContainer_searchForm_templateThird_repeaterPanel1_8_fila_field']"));
+                nombre.sendKeys(inter_tercerosEditarNombreApellidoBean.getNombre());
+            }
+
+            Thread.sleep(1000);
+
+            //ScreenShot
+            a.ScreenShotPool(driver, i, "screen1", nombreAutomatizacion);
+            Toolkit.getDefaultToolkit().beep();
+
+            //Buscar
+            WebElement buscar = driver.findElement(By.xpath("//input[@wicketpath='SearchContent_ThirdInformation_templateContainer_searchForm_searchButton']"));
+            buscar.click();
+            Thread.sleep(6000);
+
+            //Seleccionar tercero encontrado
+            WebElement seleccionTercero = driver.findElement(By.xpath("//input[@wicketpath='SearchContent_ThirdInformation_showDetailSearchTable_proof_ThirdPartyRadioGroup_resultsTable_1_thirdPartyRadio']"));
+            seleccionTercero.click();
+            Thread.sleep(2000);
+
+            //ScreenShot
+            a.ScreenShotPool(driver, i, "screen2", nombreAutomatizacion);
+            Toolkit.getDefaultToolkit().beep();
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
     }
 }
