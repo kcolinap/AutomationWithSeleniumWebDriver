947c8be69993a59e556bf8b5fd54813b929a9185
kelvin colina <kcolina@consisint.com>
2017-02-20 17:56:49 -0400

    Comienzo de proyecto automatizacion ASESUISA, TercerosConsulta

---
+++ src/AcseleV13_8Asesuisa/beans/ASESUISA_TercerosBean.java
@@ -0,0 +1,29 @@
+package AcseleV13_8Asesuisa.beans;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by kcolina on 20/02/2017.
+ */
+public class ASESUISA_TercerosBean {
+
+    private final static Logger log = Logger.getLogger(ASESUISA_TercerosBean.class);
+
+    private String tipoTercero;
+    private String aseguradoAp;
+    private String primerNombre;
+    private String segundoNombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String nacionalidad;
+    private String paisOrigen;
+    private String edadPersonaNatural;
+    private String fechaNacimiento;
+    private String numeroDUI;
+    private String numeroNIT;
+    private String numeroDocumento;
+    private String tipoDocumentoIdentificacion;
+    private String correoElectronicoPrincipal;
+    private String numeroNUP;
+    private String usuario;
+}
