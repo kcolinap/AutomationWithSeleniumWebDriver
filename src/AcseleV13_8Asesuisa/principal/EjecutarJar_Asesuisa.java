c201a0e55ce7ba8460cd4319427f7c2a9f215063
kelvin colina <kcolina@consisint.com>
2017-03-30 14:25:58 -0400

    Cambios en clase Ejecutar

--- src/AcseleV13_8Asesuisa/principal/EjecutarJar_Asesuisa.java
+++ src/AcseleV13_8Asesuisa/principal/EjecutarJar_Asesuisa.java
@@ -24,55 +24,186 @@ public class EjecutarJar_Asesuisa {
 
             switch (args[t]){
                 case "1":
-                    for (j=0; j<3; j++) {
-                        prueba = "VistaAsesuisa_BusquedaSimplePolizaTest";
-                        log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
-                        log.info("--->>> Caso #"+(j+1));
-                        try {
-                            VistaAsesuisa_BusquedaSimplePolizaTest busquedaSimplePolizaTest = new VistaAsesuisa_BusquedaSimplePolizaTest();
-                            busquedaSimplePolizaTest.setUp(j+1);
-                            busquedaSimplePolizaTest.mainTest();
-                            busquedaSimplePolizaTest.tearDown(j+1);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            log.error("Test Case - " + prueba + " - " + e);
-                        }
-                        log.info("--->>> Final de la prueba " + prueba);
+                    prueba = "VistaAsesuisa_ConsultaEdoCuentaTercerosTest";
+                    log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
+                    try {
+                        VistaAsesuisa_ConsultaEdoCuentaTercerosTest consultaEdoCuentaTercerosTest = new VistaAsesuisa_ConsultaEdoCuentaTercerosTest();
+                        consultaEdoCuentaTercerosTest.setUp(1);
+                        consultaEdoCuentaTercerosTest.mainTest();
+                        consultaEdoCuentaTercerosTest.tearDown(1);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("Test Case - " + prueba + " - " + e);
                     }
+                    log.info("--->>> Final de la prueba " + prueba);
                     break;
                 case "2":
-                    for (j=0; j<10; j++){
-                        prueba = "VistaAsesuisa_ConsultaEdoCuentaTercerosTest";
-                        log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
-                        log.info("--->>> Caso #"+(j+1));
-                        try {
-                            VistaAsesuisa_ConsultaEdoCuentaTercerosTest consultaEdoCuentaTercerosTest = new VistaAsesuisa_ConsultaEdoCuentaTercerosTest();
-                            consultaEdoCuentaTercerosTest.setUp(j+1);
-                            consultaEdoCuentaTercerosTest.mainTest();
-                            consultaEdoCuentaTercerosTest.tearDown(j+1);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            log.error("Test Case - " + prueba + " - " + e);
-                        }
-                        log.info("--->>> Final de la prueba " + prueba);
+                    prueba = "VistaAsesuisa_ConsultaEdoCuentaTercerosTest";
+                    log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
+                    try {
+                        VistaAsesuisa_ConsultaEdoCuentaTercerosTest consultaEdoCuentaTercerosTest = new VistaAsesuisa_ConsultaEdoCuentaTercerosTest();
+                        consultaEdoCuentaTercerosTest.setUp(2);
+                        consultaEdoCuentaTercerosTest.mainTest();
+                        consultaEdoCuentaTercerosTest.tearDown(2);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("Test Case - " + prueba + " - " + e);
                     }
+                    log.info("--->>> Final de la prueba " + prueba);
                     break;
                 case "3":
-                    for (j=0; j<8; j++) {
-                        prueba = "VistaAsesuisa_CancelacionAnulacionPolizaTest";
-                        log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
-                        log.info("--->>> Caso #" + (j + 1));
-                        try {
-                            VistaAsesuisa_CancelacionAnulacionPolizaTest cancelacionAnulacionPolizaTest = new VistaAsesuisa_CancelacionAnulacionPolizaTest();
-                            cancelacionAnulacionPolizaTest.setUp(j + 1);
-                            cancelacionAnulacionPolizaTest.mainTest();
-                            cancelacionAnulacionPolizaTest.tearDown(j + 1);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            log.error("Test Case - " + prueba + " - " + e);
-                        }
-                        log.info("--->>> Final de la prueba " + prueba);
+                    prueba = "VistaAsesuisa_ConsultaEdoCuentaTercerosTest";
+                    log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
+                    try {
+                        VistaAsesuisa_ConsultaEdoCuentaTercerosTest consultaEdoCuentaTercerosTest = new VistaAsesuisa_ConsultaEdoCuentaTercerosTest();
+                        consultaEdoCuentaTercerosTest.setUp(3);
+                        consultaEdoCuentaTercerosTest.mainTest();
+                        consultaEdoCuentaTercerosTest.tearDown(3);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("Test Case - " + prueba + " - " + e);
                     }
+                    log.info("--->>> Final de la prueba " + prueba);
+                    break;
+                case "4":
+                    prueba = "VistaAsesuisa_ConsultaEdoCuentaTercerosTest";
+                    log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
+                    try {
+                        VistaAsesuisa_ConsultaEdoCuentaTercerosTest consultaEdoCuentaTercerosTest = new VistaAsesuisa_ConsultaEdoCuentaTercerosTest();
+                        consultaEdoCuentaTercerosTest.setUp(4);
+                        consultaEdoCuentaTercerosTest.mainTest();
+                        consultaEdoCuentaTercerosTest.tearDown(4);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("Test Case - " + prueba + " - " + e);
+                    }
+                    log.info("--->>> Final de la prueba " + prueba);
+                    break;
+                case "5":
+                    prueba = "VistaAsesuisa_ConsultaEdoCuentaTercerosTest";
+                    log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
+                    try {
+                        VistaAsesuisa_ConsultaEdoCuentaTercerosTest consultaEdoCuentaTercerosTest = new VistaAsesuisa_ConsultaEdoCuentaTercerosTest();
+                        consultaEdoCuentaTercerosTest.setUp(5);
+                        consultaEdoCuentaTercerosTest.mainTest();
+                        consultaEdoCuentaTercerosTest.tearDown(5);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("Test Case - " + prueba + " - " + e);
+                    }
+                    log.info("--->>> Final de la prueba " + prueba);
+                    break;
+                case "6":
+                    prueba = "VistaAsesuisa_ConsultaEdoCuentaTercerosTest";
+                    log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
+                    try {
+                        VistaAsesuisa_ConsultaEdoCuentaTercerosTest consultaEdoCuentaTercerosTest = new VistaAsesuisa_ConsultaEdoCuentaTercerosTest();
+                        consultaEdoCuentaTercerosTest.setUp(6);
+                        consultaEdoCuentaTercerosTest.mainTest();
+                        consultaEdoCuentaTercerosTest.tearDown(6);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("Test Case - " + prueba + " - " + e);
+                    }
+                    log.info("--->>> Final de la prueba " + prueba);
+                    break;
+                case "7":
+                    prueba = "VistaAsesuisa_ConsultaEdoCuentaTercerosTest";
+                    log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
+                    try {
+                        VistaAsesuisa_ConsultaEdoCuentaTercerosTest consultaEdoCuentaTercerosTest = new VistaAsesuisa_ConsultaEdoCuentaTercerosTest();
+                        consultaEdoCuentaTercerosTest.setUp(7);
+                        consultaEdoCuentaTercerosTest.mainTest();
+                        consultaEdoCuentaTercerosTest.tearDown(7);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("Test Case - " + prueba + " - " + e);
+                    }
+                    log.info("--->>> Final de la prueba " + prueba);
+                    break;
+                case "8":
+                    prueba = "VistaAsesuisa_ConsultaEdoCuentaTercerosTest";
+                    log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
+                    try {
+                        VistaAsesuisa_ConsultaEdoCuentaTercerosTest consultaEdoCuentaTercerosTest = new VistaAsesuisa_ConsultaEdoCuentaTercerosTest();
+                        consultaEdoCuentaTercerosTest.setUp(8);
+                        consultaEdoCuentaTercerosTest.mainTest();
+                        consultaEdoCuentaTercerosTest.tearDown(8);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("Test Case - " + prueba + " - " + e);
+                    }
+                    log.info("--->>> Final de la prueba " + prueba);
+                    break;
+                case "988":
+                    prueba = "VistaAsesuisa_ConsultaEdoCuentaTercerosTest";
+                    log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
+                    try {
+                        VistaAsesuisa_ConsultaEdoCuentaTercerosTest consultaEdoCuentaTercerosTest = new VistaAsesuisa_ConsultaEdoCuentaTercerosTest();
+                        consultaEdoCuentaTercerosTest.setUp(9);
+                        consultaEdoCuentaTercerosTest.mainTest();
+                        consultaEdoCuentaTercerosTest.tearDown(9);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("Test Case - " + prueba + " - " + e);
+                    }
+                    log.info("--->>> Final de la prueba " + prueba);
+                    break;
+                case "10":
+                    prueba = "VistaAsesuisa_ConsultaEdoCuentaTercerosTest";
+                    log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
+                    try {
+                        VistaAsesuisa_ConsultaEdoCuentaTercerosTest consultaEdoCuentaTercerosTest = new VistaAsesuisa_ConsultaEdoCuentaTercerosTest();
+                        consultaEdoCuentaTercerosTest.setUp(10);
+                        consultaEdoCuentaTercerosTest.mainTest();
+                        consultaEdoCuentaTercerosTest.tearDown(10);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("Test Case - " + prueba + " - " + e);
+                    }
+                    log.info("--->>> Final de la prueba " + prueba);
+                    break;
+                case "11":
+                    prueba = "VistaAsesuisa_BusquedaSimplePolizaTest";
+                    log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
+                    try {
+                        VistaAsesuisa_BusquedaSimplePolizaTest busquedaSimplePolizaTest = new VistaAsesuisa_BusquedaSimplePolizaTest();
+                        busquedaSimplePolizaTest.setUp(1);
+                        busquedaSimplePolizaTest.mainTest();
+                        busquedaSimplePolizaTest.tearDown(1);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("Test Case - " + prueba + " - " + e);
+                    }
+                    log.info("--->>> Final de la prueba " + prueba);
+                    break;
+                case "12":
+                    prueba = "VistaAsesuisa_BusquedaSimplePolizaTest";
+                    log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
+                    try {
+                        VistaAsesuisa_BusquedaSimplePolizaTest busquedaSimplePolizaTest = new VistaAsesuisa_BusquedaSimplePolizaTest();
+                        busquedaSimplePolizaTest.setUp(2);
+                        busquedaSimplePolizaTest.mainTest();
+                        busquedaSimplePolizaTest.tearDown(2);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("Test Case - " + prueba + " - " + e);
+                    }
+                    log.info("--->>> Final de la prueba " + prueba);
+                    break;
+                case "13":
+                    prueba = "VistaAsesuisa_BusquedaSimplePolizaTest";
+                    log.info("--->>> Ejecutando la prueba " + args[t] + " " + prueba);
+                    try {
+                        VistaAsesuisa_BusquedaSimplePolizaTest busquedaSimplePolizaTest = new VistaAsesuisa_BusquedaSimplePolizaTest();
+                        busquedaSimplePolizaTest.setUp(3);
+                        busquedaSimplePolizaTest.mainTest();
+                        busquedaSimplePolizaTest.tearDown(3);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("Test Case - " + prueba + " - " + e);
+                    }
+                    log.info("--->>> Final de la prueba " + prueba);
                     break;
                 default:
                     log.info("--->>> No se ha seleccionado ninguna prueba o el valor de la misma es incorrecto");
