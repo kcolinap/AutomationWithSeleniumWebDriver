a87a2cea3708b1bca3a4516ff65a9ce3144c52fb
kelvin colina <kcolina@consisint.com>
2017-03-31 17:53:20 -0400

    Correccion clase ejecutar

--- src/AcseleV13_8Asesuisa/principal/EjecutarJar_Asesuisa.java
+++ src/AcseleV13_8Asesuisa/principal/EjecutarJar_Asesuisa.java
@@ -15,7 +15,7 @@ public class EjecutarJar_Asesuisa {
 
     public static void main(String[] args) {
 
-        args = new String[]{"4", "4", "1"};
+        //args = new String[]{"4", "4", "1"};
         //System.out.println("El tamaño del arreglo es: " + args.length);
 
         String primero = args[0];
