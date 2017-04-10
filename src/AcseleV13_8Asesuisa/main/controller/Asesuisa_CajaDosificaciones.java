540021643c9044a6fe88b8ef6fcdef3d139bec38
Arnaldo Andrade <aandrade@consisint.com>
2017-04-10 16:17:58 -0400

    Avance Dosificaciones de Caja

--- src/AcseleV13_8Asesuisa/main/controller/Asesuisa_CajaDosificaciones.java
+++ src/AcseleV13_8Asesuisa/main/controller/Asesuisa_CajaDosificaciones.java
@@ -2,15 +2,11 @@ package AcseleV13_8Asesuisa.main.controller;
 
 import AcseleV13_8Asesuisa.beans.Asesuisa_CajaDosificacionesBean;
 import AcseleV13_8Asesuisa.main.controller.Asesuisa_Menu.Asesuisa_MenuMantenimiento;
-import AcseleV13_8Asesuisa.main.controller.Asesuisa_Menu.Asesuisa_MenuOperaciones;
 import metodo.Metodos;
 import org.apache.log4j.Logger;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 
 /**
  * Created by aandrade on 06/04/2017.
@@ -46,13 +42,13 @@ public class Asesuisa_CajaDosificaciones {
             DosificacionesCaja(bean, a, i, folderName, 3, 4, 5, 6, 7);
             Thread.sleep(3000);
 
-            driver.quit();
+           // driver.quit();
 
         } catch (Exception e) {
             e.printStackTrace();
             log.info("Test Case - " + nombreAutomatizacion + " - " + e);
             if (driver != null){
-                driver.quit();
+                //driver.quit();
             }
         }
     }
@@ -66,7 +62,7 @@ public class Asesuisa_CajaDosificaciones {
 
             // Si no se selecciona Creacion Generica
            if(bean.getGenerica() == null) {
-
+               // Modalidad de Factura
                if (bean.getModfactura() != null) {
                    driver.findElement(By.xpath("//*[@id='gwt-uid-8']")).click();
                    Thread.sleep(1000);
@@ -79,7 +75,7 @@ public class Asesuisa_CajaDosificaciones {
                        }
                    }
                }
-
+               // Compañia de Tiraje
                if (bean.getComtiraje() != null) {
                    driver.findElement(By.xpath("//*[@id=\'gwt-uid-10\']")).click();
                    Thread.sleep(1000);
@@ -92,12 +88,69 @@ public class Asesuisa_CajaDosificaciones {
                        }
                    }
                }
-
+               // Año del Tiraje
                if (bean.getAnotiraje() != null) {
                    WebElement anotiraje = driver.findElement(By.xpath("//*[@id='gwt-uid-12']"));
                    anotiraje.sendKeys(bean.getAnotiraje());
                }
+               // Certificado de Hacienda
+               if (bean.getCertiraje() != null) {
+                   WebElement certiraje = driver.findElement(By.xpath("//*[@id=\'CertificadoHacienda\']"));
+                   certiraje.sendKeys(bean.getCertiraje());
+               }
 
+               Thread.sleep(1000);
+               a.ScreenShotPool(driver, i, "screen" + numScreenShoot, nombreAutomatizacion, folderName);
+               Thread.sleep(1000);
+
+               driver.findElement(By.xpath("//*[@id=\"WControllervaadinservlet-1750660287\"]/div/div[2]/div[2]/div/div[2]/div/div[5]/div/div/div/span")).click();
+
+               Thread.sleep(1000);
+               a.ScreenShotPool(driver, i, "screen" + numScreenShoot2, nombreAutomatizacion, folderName);
+               Thread.sleep(1000);
+
+           }
+            // Si se selecciona Creacion Generica
+            else  {
+               driver.findElement(By.xpath("//*[@id=\"gwt-uid-2\"]")).click();
+               Thread.sleep(2000);
+               // Si se ingresa rango de factura desde
+               if (bean.getRangodesd() != null) {
+                   WebElement rangodesd = driver.findElement(By.xpath("//*[@id=\'gwt-uid-16\']"));
+                   rangodesd.sendKeys(bean.getRangodesd());
+               }
+               else {
+                   WebElement rangodesd = driver.findElement(By.xpath("//*[@id=\'gwt-uid-16\']"));
+                   rangodesd.clear();
+               }
+               // Si se ingresa rango de factura hasta
+               if (bean.getRangohast() != null) {
+                   WebElement rangohast = driver.findElement(By.xpath("//*[@id=\'gwt-uid-18\']"));
+                   rangohast.sendKeys(bean.getRangohast());
+               }
+               else {
+                   WebElement rangohast = driver.findElement(By.xpath("//*[@id=\'gwt-uid-18\']"));
+                   rangohast.clear();
+               }
+               // Compañia de Tiraje
+               if (bean.getComtiraje() != null) {
+                   driver.findElement(By.xpath("//*[@id=\'gwt-uid-10\']")).click();
+                   Thread.sleep(1000);
+                   tamanotr = driver.findElements(By.xpath("//*[@id=\'VAADIN_COMBOBOX_OPTIONLIST\']/div/div[2]/table/tbody/tr")).size();
+                   for (int j = 0; j < tamanotr; j++) {
+                       indextr = driver.findElements(By.xpath("//*[@id=\'VAADIN_COMBOBOX_OPTIONLIST\']/div/div[2]/table/tbody/tr")).get(j);
+                       if (bean.getComtiraje().equals(indextr.getText())) {
+                           indextr.click();
+                           break;
+                       }
+                   }
+               }
+               // Año del Tiraje
+               if (bean.getAnotiraje() != null) {
+                   WebElement anotiraje = driver.findElement(By.xpath("//*[@id='gwt-uid-12']"));
+                   anotiraje.sendKeys(bean.getAnotiraje());
+               }
+               // Certificado de Hacienda
                if (bean.getCertiraje() != null) {
                    WebElement certiraje = driver.findElement(By.xpath("//*[@id=\'CertificadoHacienda\']"));
                    certiraje.sendKeys(bean.getCertiraje());
@@ -115,57 +168,6 @@ public class Asesuisa_CajaDosificaciones {
 
            }
 
-
-
-
-
-
-
-
-
-
-              // WebElement generica = driver.findElement(By.xpath("//*[@id=\'gwt-uid-2\']"));
-              // generica.click();
-/*
-               Thread.sleep(1000);
-               Select generica = new Select(driver.findElement(By.xpath("//*[@id=\"gwt-uid-2\"]")));
-               generica.selectByValue(bean.getGenerica());
-
-                Thread.sleep(1000);
-                a.ScreenShotPool(driver, i, "screen" + numScreenShoot, nombreAutomatizacion, folderName);
-                Thread.sleep(1000);
-
-                driver.findElement(By.xpath("//input[@value='Aceptar']")).click();
-
-                // Mensajes de Resultado de la operacion
-                Thread.sleep(1000);
-                a.ScreenShotPool(driver, i, "screen" + numScreenShoot, nombreAutomatizacion, folderName);
-                Thread.sleep(1000);
-
-            // Si no se selecciona el campo: Nro. de caja
-            else if (bean.getNrocaja() == null){
-
-                Thread.sleep(1000);
-                a.ScreenShotPool(driver, i, "screen" + numScreenShoot, nombreAutomatizacion, folderName);
-                Thread.sleep(1000);
-
-                //String acselurl=(driver.getCurrentUrl().substring(0, 20));
-                driver.findElement(By.xpath("//input[@value='Aceptar']")).click();
-
-                // Mensajes de Alerta JavaScript
-                if (ExpectedConditions.alertIsPresent() != null) {
-                    Thread.sleep(1000);
-                    Alert alert = driver.switchTo().alert();
-                    String alertmess = alert.getText();
-                    a.ScreenShotJavaScript(acselurl, i, nombreAutomatizacion, folderName, "screen" + numScreenShoot2 );
-                    alert.accept();
-                    //System.out.println(alertmess);
-                    Thread.sleep(1000);
-                    driver.switchTo().defaultContent();
-                }
-
-            }
-*/
         } catch (Exception e) {
             e.printStackTrace();
             log.info("Test Case - " + nombreAutomatizacion + " - " + e);
